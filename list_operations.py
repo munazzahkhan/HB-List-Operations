a79bc78da33dad8a2decf27cb4525f089ddb3b26
Munazzah Hassan Khan <munazzah.hassan@gmail.com>
2021-04-29 16:59:20 +0000

    Completes the 'last_five' function

--- list_operations.py
+++ list_operations.py
@@ -62,7 +62,7 @@ def first_three(input_list):
 
     """
 
-    return [input_list[0],input_list[1],input_list[2]]
+    return input_list[0:3]
 
 
 def last_five(input_list):
@@ -75,7 +75,7 @@ def last_five(input_list):
 
     """
 
-    return []
+    return input_list[-5:]
 
 
 def middle(input_list):
